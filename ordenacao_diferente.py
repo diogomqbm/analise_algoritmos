128bd5458896526928cbeea35d10d8e8778bc6a2
Luisa <luisaamartins10@gmail.com>
2021-09-18 16:47:59 -0300

    fix ordenacao_diferente

--- ordenacao_diferente.py
+++ ordenacao_diferente.py
@@ -1,11 +1,11 @@
-intervalo = list(range(0,2000001))
+intervalo = list(range(0,200))
 res = sorted(intervalo, key=lambda ele: len(
     [ele for idx in range(1, ele) if ele % idx == 0]))
 casos = []
 while True:
     caso = int(input())
-    casos.append(caso)
     if caso == 0:
         break
+    casos.append(caso)
 for i in casos:
     print("Caso {}: {}".format(i, res.index(i)))
