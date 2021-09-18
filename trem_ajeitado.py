03ac7cb009f186d525105775a4bba70acad04d96
Luisa <luisaamartins10@gmail.com>
2021-09-18 17:00:25 -0300

    trying to fix trem_ajeitado

--- trem_ajeitado.py
+++ trem_ajeitado.py
@@ -1,4 +1,4 @@
-def count_ordena_vagoes(array_vagoes):
+def ordena_vagoes(array_vagoes):
     num_inversoes = 0
     n = len(array_vagoes)
     for i in range(n):
@@ -12,12 +12,11 @@ num_casos_teste = int(input())
 lista_ajeitamentos = []
 for i in range(num_casos_teste):
     num_vagoes = int(input())
-    if num_vagoes == 0:
-        break
-    vagoes = list(map(int, input().split()))
-    if len(vagoes) != num_vagoes:
-        exit()
-    num_ajeitamentos = count_ordena_vagoes(vagoes)
-    lista_ajeitamentos.append(num_ajeitamentos)
+    if num_vagoes > 0:
+        vagoes = list(map(int, input().split()))
+        if len(vagoes) != num_vagoes:
+            exit()
+        num_ajeitamentos = ordena_vagoes(vagoes)
+        lista_ajeitamentos.append(num_ajeitamentos)
 for i in lista_ajeitamentos:
     print('A quantidade ajeitamentos deve ser {}.'.format(i))
--- trem_ajeitado2.py
+++ trem_ajeitado2.py
@@ -8,7 +8,6 @@ def insertion_sort(arr):
                 arr[j + 1] = arr[j]
                 j -= 1
         arr[j + 1] = key
-
     return count
  
 
@@ -16,9 +15,11 @@ num_casos_teste = int(input())
 for i in range(num_casos_teste):
     lista_ajeitamentos = []
     num_vagoes = int(input())
-    vagoes = list(map(int, input().split()))
-    if len(vagoes) != num_vagoes:
-        exit()
-    num_ajeitamentos = insertion_sort(vagoes)
-    lista_ajeitamentos.append(num_ajeitamentos)
-    print('A quantidade ajeitamentos deve ser ' + str(num_ajeitamentos) + '.')
+    if num_vagoes > 0:
+        vagoes = list(map(int, input().split()))
+        if len(vagoes) != num_vagoes:
+            exit()
+        num_ajeitamentos = insertion_sort(vagoes)
+        lista_ajeitamentos.append(num_ajeitamentos)
+for i in lista_ajeitamentos:
+    print('A quantidade ajeitamentos deve ser {}.'.format(i))
